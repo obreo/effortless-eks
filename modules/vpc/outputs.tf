8a3a43e9428a1418453fd40fc99d5dda79feb2fc
Ibrahim Khader <i.khader@tigercrypto.uk>
2025-06-05 11:34:01 +0500

    included ipv6 support

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,6 +1,6 @@
 # Security groups:
 output "cluster_security_group_id" {
-  value = aws_security_group.cluster[0].id #data.aws_security_groups.cluster.id
+  value = {for k, v in aws_security_group.cluster : k => v.id} #data.aws_security_groups.cluster.id
 }
 output "public_subnet_cidr_blocks" {
   value = aws_subnet.public[*].id # Replace with the actual resource type in your VPC module

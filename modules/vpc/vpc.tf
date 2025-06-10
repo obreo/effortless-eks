eb50f87c2e7bd3c49dd344eb286d2c06d3c42fc1
Ibrahim Khader <i.khader@tigercrypto.uk>
2025-06-10 21:00:08 +0500

    updated outputs

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -32,7 +32,7 @@ resource "aws_subnet" "public" {
   }
 }
 resource "aws_subnet" "private" {
-  count                                           = var.vpc_settings.enable_aws_ipv6_cidr_block.ipv6_native ? var.vpc_settings.enable_aws_ipv6_cidr_block.private_cidr_count_prefix64 : length(var.vpc_settings.private_subnet_cidr_blocks)
+  count                                           = var.vpc_settings.enable_aws_ipv6_cidr_block.ipv6_native || var.vpc_settings.enable_aws_ipv6_cidr_block.private_cidr_count_prefix64 != 0 ? var.vpc_settings.enable_aws_ipv6_cidr_block.private_cidr_count_prefix64 : length(var.vpc_settings.private_subnet_cidr_blocks)
   vpc_id                                          = aws_vpc.vpc.id
   cidr_block                                      = var.vpc_settings.enable_aws_ipv6_cidr_block.ipv6_native ? null : try(var.vpc_settings.private_subnet_cidr_blocks[count.index], null)
   ipv6_cidr_block                                 = try(var.vpc_settings.enable_aws_ipv6_cidr_block.private_cidr_count_prefix64 != 0 ? cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 8, count.index) : null)
